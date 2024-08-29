9661d1f5d10cc04ab70011acd6ba11a7787ddcb5
JoaoGustavoMr <joaogustavo2202@gmail.com>
2024-08-29 16:01:41 -0300

    Commitão Joao G

--- script/simulador.js
+++ script/simulador.js
@@ -1,12 +1,8 @@
-// Seleciona o elemento range e o elemento onde o valor será exibido
 const rangeInput = document.getElementById('prazoMeses');
 const valueDisplay = document.getElementById('prazoValor');
 
-// Define o valor inicial na exibição
 valueDisplay.textContent = `${rangeInput.value} meses`;
 
-// Adiciona um ouvinte de eventos para atualizar o valor exibido
 rangeInput.addEventListener('input', function() {
     valueDisplay.textContent = `${this.value} meses`;
 });
-

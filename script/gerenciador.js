c1decb5c46231e52b38af64dfb52a39abef92893
Kaique Ferreira <kaique1245br@gmail.com>
2024-09-05 06:33:24 -0300

    Gerenciador quase finalizado

--- script/gerenciador.js
+++ script/gerenciador.js
@@ -1,26 +1,118 @@
 function openModal() {
-    const modal = document.getElementById('modal-container')
-    modal.classList.add('mostrar')
+    document.getElementById("modal-container").classList.add("mostrar");
+}
+const KEY_BD = "@usuarioestudo"
 
-    modal.addEventListener('click', (e) => {
-        if (e.target.id == "modal-container" || e.target.id == "close-modal") {
-            modal.classList.remove('mostrar')
-        }
-    })
+function gravarBD() {
+    localStorage.setItem(KEY_BD, JSON.stringify(listaRegistros));
 }
 
-function openModal2() {
-    const modal = document.getElementById('modal-container2')
-    modal.classList.add('mostrar')
-
-    modal.addEventListener('click', (e) => {
-        if (e.target.id == "modal-container2" || e.target.id == "close-modal") {
-            modal.classList.remove('mostrar')
-        }
-    })
+function lerBD() {
+    const data = localStorage.getItem(KEY_BD);
+    if (data) {
+        listaRegistros = JSON.parse(data);
+    }
+    render();
 }
 
-function openAlert() {
+function editar(index) {
+    const usuario = listaRegistros.usuarios[index];
+    if (usuario) {
+        document.getElementById('editTextUsuario').value = usuario.textUsuario;
+        document.getElementById('editNumber').value = usuario.number;
+        document.getElementById('editDate').value = usuario.date;
+
+        document.getElementById('indexEditando').value = index;
+
+        document.getElementById('modal-container2').classList.add('mostrar');
+    }
+}
+
+function render() {
+    const tbody = document.getElementById("listaRegistrosBody");
+    if (tbody) {
+        tbody.innerHTML = listaRegistros.usuarios.map((usuario, index) => {
+
+            const numero = parseFloat(usuario.number);
+            const estilo = numero < 0 ? 'style="color: red;"' : 'style="color: green;"';
+            var numeroFormatado = numero >= 0 ? `+R$${Math.abs(numero).toFixed(2)}` : `-R$${Math.abs(numero).toFixed(2)}`;
+            
+            return `
+            <tr>
+                <td>${usuario.textUsuario}</td>
+                <td ${estilo}>${numeroFormatado}</td>
+                <td>${usuario.date}</td>
+                <td><img onclick="editar(${index})" src="../img/editar.png" alt="Editar"></td>
+                <td><svg onclick="deletar(${index})" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14 2C7.37258 2 2 7.37258 2 14C2 20.6274 7.37258 26 14 26C20.6274 26 26 20.6274 26 14C26 7.37258 20.6274 2 14 2Z" stroke="#E83F5B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M7.57143 15.0713C6.97969 15.0713 6.5 14.5916 6.5 13.9999V13.9999C6.5 13.4082 6.97969 12.9285 7.57143 12.9285L20.4286 12.9285C21.0203 12.9285 21.5 13.4082 21.5 13.9999V13.9999C21.5 14.5916 21.0203 15.0713 20.4286 15.0713L7.57143 15.0713Z" fill="#E83F5B"/>
+                    </svg></td>
+            </tr>`;
+        }).join("");
+
+        calcular();
+    }
+}
+
+
+function calcular(){
+    let entrada = 0
+    let saida = 0
+    
+    listaRegistros.usuarios.forEach(usuario => {
+        const numero = parseInt(usuario.number)
+        
+        if (numero >= 0){
+            entrada += numero
+        }
+        else{
+            saida += numero
+        }
+    });
+    
+    
+    let total = entrada - saida
+
+    document.getElementById("entrada").textContent = `R$ ${entrada.toFixed(2)}`
+    document.getElementById("saida").textContent = `R$ ${saida.toFixed(2)}`
+    document.getElementById("total").textContent = `R$ ${total.toFixed(2)}`
+}
+
+
+function insertUsuario(textUsuario, number, date) {
+    listaRegistros.ultimoIdGerado++;
+    listaRegistros.usuarios.push({ textUsuario, number, date });
+
+    limparEdicao();
+    gravarBD();
+    render();
+
+    document.getElementById("modal-container").classList.remove("mostrar");
+}
+
+
+
+function salvarEdicao() {
+    const index = document.getElementById('indexEditando').value;
+
+    if (index !== "") {
+        listaRegistros.usuarios[index] = {
+            textUsuario: document.getElementById('editTextUsuario').value,
+            number: document.getElementById('editNumber').value,
+            date: document.getElementById('editDate').value
+        }
+
+        limparEdicao();
+        gravarBD();
+        render();
+
+        document.getElementById('modal-container2').classList.remove('mostrar');
+    }
+}
+
+
+
+function deletar(index) {
     Swal.fire({
         title: "Are you sure?",
         text: "You won't be able to revert this!",
@@ -29,67 +121,57 @@ function openAlert() {
         confirmButtonColor: "#3085d6",
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!"
-      }).then((result) => {
+    }).then((result) => {
         if (result.isConfirmed) {
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success"
-          });
+            listaRegistros.usuarios.splice(index, 1)
+
+            Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+            });
+            gravarBD();
+            render();
         }
-      });
+    });
+
 }
 
-// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO// CADASTRAR TRANSAÇÃO
+function limparEdicao() {
+    document.getElementById("textUsuario").value = ''
+    document.getElementById("number").value = ''
+    document.getElementById("date").value = ''
 
-var listaRegistros = {
-    ultimoIdGerado: 0,
-    usuarios: [
-        { textUsuario: "Casa", number: "12122", date: "2024-09-02" },
-        { textUsuario: "Trabalho", number: "2122", date: "2024-09-03" },
-        { textUsuario: "Financeiro", number: "32122", date: "2024-09-04" }
-    ]
+    document.getElementById("editTextUsuario").value = "";
+    document.getElementById("editNumber").value = "";
+    document.getElementById("editDate").value = "";
 }
-
-function render() {
-    const tbody = document.getElementById("listaRegistrosBody")
-    if (tbody) {
-        tbody.innerHTML = listaRegistros.usuarios.map(usuarios => {
-            return `
-            <tr>
-                <td>${usuarios.textUsuario}</td>
-                <td>R$${usuarios.number}</td>
-                <td>${usuarios.date}</td>
-                <td><img onclick="openModal2()" src="../img/editar.png" alt=""></td>
-                <td><svg onclick="openAlert()" width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M14 2C7.37258 2 2 7.37258 2 14C2 20.6274 7.37258 26 14 26C20.6274 26 26 20.6274 26 14C26 7.37258 20.6274 2 14 2Z" stroke="#E83F5B" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M7.57143 15.0713C6.97969 15.0713 6.5 14.5916 6.5 13.9999V13.9999C6.5 13.4082 6.97969 12.9285 7.57143 12.9285L20.4286 12.9285C21.0203 12.9285 21.5 13.4082 21.5 13.9999V13.9999C21.5 14.5916 21.0203 15.0713 20.4286 15.0713L7.57143 15.0713Z" fill="#E83F5B"/>
-</svg>
-                </tr>`
-
-        }).join('')
-    }
-}
-
-function insertUsuario(textUsuario, number, date) {
-    listaRegistros.ultimoIdGerado++;
-    listaRegistros.usuarios.push({ textUsuario, number, date });
-}
-
-function submeter(e) {
-    e.preventDefault()
-    const data = {
-        textUsuario: document.getElementById('textUsuario').value,
-        number: document.getElementById('number').value,
-        date: document.getElementById('date').value,
-    };
-    insertUsuario(data.textUsuario, data.number, data.date);
-    render();
-    document.getElementById('modal-container').classList.remove('mostrar');
-}
-
 
 window.addEventListener('load', () => {
-    render()
-    document.getElementById('modal-container').addEventListener('submit', submeter)
+    lerBD()
+
+    document.getElementById("save-modal").addEventListener("click", (e) => {
+
+        e.preventDefault()
+
+        const textUsuario = document.getElementById('textUsuario').value
+        const number = document.getElementById('number').value
+        const date = document.getElementById('date').value;
+        if (textUsuario && number && date) {
+            insertUsuario(textUsuario, number, date)
+        }
+    })
+
+    document.getElementById("btnSalvarEdicao").addEventListener("click", (e) => {
+        e.preventDefault();
+        salvarEdicao();
+    })
+
+    document.getElementById("close-modal").addEventListener("click", () => {
+        document.getElementById("modal-container").classList.remove("mostrar");
+    });
+
+    document.getElementById("close-modal2").addEventListener("click", () => {
+        document.getElementById("modal-container2").classList.remove("mostrar");
+    });
 })
